30cd6fcf99dfd48b847df0ace3edc0db557bf4c4
marco <marco.morandin@studenti.unitn.it>
2022-09-18 13:37:15 +0200

    Lezione1

--- cmake-build-debug/CMakeFiles/Lezione1.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Lezione1.dir/DependInfo.cmake
@@ -1,16 +1,15 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/mnt/c/Users/emmee/Desktop/Lezione1/Persona.cpp" "/mnt/c/Users/emmee/Desktop/Lezione1/cmake-build-debug/CMakeFiles/Lezione1.dir/Persona.cpp.o"
-  "/mnt/c/Users/emmee/Desktop/Lezione1/main.cpp" "/mnt/c/Users/emmee/Desktop/Lezione1/cmake-build-debug/CMakeFiles/Lezione1.dir/main.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/marco/CLionProjects/ProgrammazioneAvanzata/Persona.cpp" "CMakeFiles/Lezione1.dir/Persona.cpp.o" "gcc" "CMakeFiles/Lezione1.dir/Persona.cpp.o.d"
+  "/Users/marco/CLionProjects/ProgrammazioneAvanzata/main.cpp" "CMakeFiles/Lezione1.dir/main.cpp.o" "gcc" "CMakeFiles/Lezione1.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links.
--- cmake-build-debug/CMakeFiles/Lezione1.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Lezione1.dir/cmake_clean.cmake
@@ -1,6 +1,8 @@
 file(REMOVE_RECURSE
   "CMakeFiles/Lezione1.dir/Persona.cpp.o"
+  "CMakeFiles/Lezione1.dir/Persona.cpp.o.d"
   "CMakeFiles/Lezione1.dir/main.cpp.o"
+  "CMakeFiles/Lezione1.dir/main.cpp.o.d"
   "Lezione1"
   "Lezione1.pdb"
 )
